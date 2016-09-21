f4885e8797948c672b2eac36791f02b48b723fd6
AntonieManini <antoxa3030@yandex.ru>
2016-09-21 10:25:08 +0200

    Implemented concurrent matrix multiplication

--- src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
+++ src/main/java/ru/javaops/masterjava/matrix/MatrixUtil.java
@@ -11,8 +11,49 @@ import java.util.concurrent.ExecutorService;
 public class MatrixUtil {
 
     // TODO implement parallel multiplication matrixA*matrixB
-    public static int[][] concurrentMultiply(int[][] matrixA, int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
-        return null;
+    public static int[][] concurrentMultiply(final int[][] matrixA, final int[][] matrixB, ExecutorService executor) throws InterruptedException, ExecutionException {
+        final int matrixSize = matrixA.length;
+        final int[][] matrixC = new int[matrixSize][matrixSize];
+        final int[][] matrixBT = new int[matrixSize][matrixSize];
+        List<Future<Boolean>> futures = new LinkedList<Future<Boolean>>();
+        CompletionService<Boolean> completionService = new ExecutorCompletionService<Boolean>(executor);
+
+        for (int i = 0; i < matrixSize; i++ ) {
+            for (int j = 0; j < matrixSize; j++) {
+                matrixBT[j][i] = matrixB[i][j];
+            }
+        }
+
+        for (int i = 0; i < matrixSize; i++) {
+            for (int j = 0; j < matrixSize; j++) {
+                final int row = i;
+                final int column = j;
+
+                futures.add(completionService.submit(new Callable<Boolean>() {
+                    @Override
+                    public Boolean call() {
+                        int sum = 0;
+
+                        for (int k = 0; k < matrixSize; k++) {
+                            sum += matrixA[row][k] * matrixBT[column][k];
+                        }
+
+                        matrixC[row][column] = sum;
+
+                        return true;
+                    }
+                }));
+            }
+        }
+
+        while (!futures.isEmpty()) {
+            Future<Boolean> completed = completionService.poll();
+            if (completed != null) {
+                futures.remove(completed);
+            }
+        }
+
+        return matrixC;
     }
 
     public static int[][] singleThreadMultiply(int[][] matrixA, int[][] matrixB) {
